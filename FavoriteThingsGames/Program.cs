973bcfc83609b535436cfdc28c80aba4adc029bd
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-14 14:34:22 -0700

    first commit, working progress

---
+++ FavoriteThingsGames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FavoriteThingsGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! You again? Want to play, huh? Alright. Guess my favorite number between 1 and 100 then. Take a guess!");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+            var userInput = int.Parse(Console.ReadLine());
+
+            
+            {
+                if (userInput == favNumber)
+                {
+                    Console.WriteLine("WOW! Congratulations! This is correct");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else
+                {
+                    Console.WriteLine("Too low!");
+                }
+            }
+
+        }
+    }
+}
